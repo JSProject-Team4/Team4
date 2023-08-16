0a0ab38e5267f4bce73fdd1155e313bb2c88aa90
다빈 <prospecs1039@daum.net>
2023-08-16 22:26:42 +0900

    draw 함수를 draw와 drawBall 함수로 분해정리

--- DaveenGame2/index.js
+++ DaveenGame2/index.js
@@ -7,18 +7,24 @@ const ctx = canvas.getContext('2d');
 // 공의 시작위치 지정 ( 화면 중앙 + 하단부터 30px 윗칸)
 let x = canvas.width / 2;
 let y = canvas.height - 30;
-// 공의 좌표를 계속 변하게 해야함
+// 공의 좌표를 계속 변하게 하기 위해서 프레임마다 x와 y에 추가될 값
 let dx = 2;
 let dy = -2;
 
 // 그리기 함수 (10밀리초마다 호출됨 = 무한작동)
 function draw() {
-  ctx.beginPath();  // 그리기 시작
-  ctx.arc(x, y, 10, 0, Math.PI * 2); // x: 50, y: 50, 반지름: 10, 시작각도: 0, 끝 각도: 3.14 * 2 
-  ctx.fillStyle = '#0095DD';
-  ctx.fill();
-  ctx.closePath();
-  x += dx;
-  y += dy;
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // 공을 그리기 전에 기존에 공을 지운다
+  drawBall();  // 주인"공" 그리기 함수 호출
+  x += dx;   // x의 값 +2
+  y += dy;   // y의 값 -2
 }
-setInterval(draw, 10);
+
+// 주인"공"을 그리는 함수
+function drawBall() {
+    ctx.beginPath();  // 그리기 시작
+    ctx.arc(x, y, 10, 0, Math.PI * 2); // x: 50, y: 50, 반지름: 10, 시작각도: 0, 끝 각도: 3.14 * 2 
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();  // 그리기 끝
+}
+setInterval(draw, 10); // 10밀리초 간격 반복호출
