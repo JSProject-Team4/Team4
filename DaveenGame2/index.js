41dd8c7f6b2d920b36bf2eb2baea054c6c98b9bb
daveen99 <prospecs1039@daum.net>
2023-08-17 14:09:27 +0900

    라켓으로 주인"공"을 튕길 수 있게함

--- DaveenGame2/index.js
+++ DaveenGame2/index.js
@@ -7,7 +7,7 @@ const ctx = canvas.getContext('2d');
 
 // 공의 시작위치 지정 ( 화면 중앙 + 하단부터 30px 윗칸)
 let x = canvas.width / 2;
-let y = canvas.height - 30;
+let y = canvas.height - 60;
 
 // 공의 반지름
 const ballRadius = 10;
@@ -21,6 +21,7 @@ let MovingY = -2;
 let racketHeight = 10;
 let racketWidth = 75;
 let racketX = (canvas.width - racketWidth) / 2; // canvas의 길이에서 라켓길이를 빼고 그것의 반절의 위치
+let racketY = canvas.height - 40;
 
 // 좌, 우 키보드 입력변수
 let rightKey = false;
@@ -40,7 +41,7 @@ function drawBall() {
 // 라켓을 그리는 함수
 function drawRacket() {
   ctx.beginPath();
-  ctx.rect(racketX, canvas.height - racketHeight, racketWidth, racketHeight);
+  ctx.rect(racketX, racketY, racketWidth, racketHeight);
   ctx.fillStyle = '#0095DD';
   ctx.fill();
   ctx.closePath();
@@ -49,9 +50,12 @@ function drawRacket() {
 // 게임을 리셋하는 함수
 function resetGame() {
   x = canvas.width / 2;
-  y = canvas.height - 30;
+  y = canvas.height - 60;
   movingX = 2;
   MovingY = -2;
+
+  racketX = (canvas.width - racketWidth) / 2; // canvas의 길이에서 라켓길이를 빼고 그것의 반절의 위치
+  racketY = canvas.height - 40;
 }
 
 // 그리기 함수 (10밀리초마다 호출됨 = 무한작동)
@@ -59,7 +63,6 @@ function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // 공을 그리기 전에 기존에 공을 지운다
   drawBall(); // 주인"공" 그리기 함수 호출
   drawRacket(); // 라켓 그리기 함수 호출
-
   // ============ 주인"공"을 벽에 튕기게 하는 파트 ============ //
   // 만약에 현재의 높이에서 + MovingY( -2 )를 했을때 0보다 작아지는경우(벽을넘어감)
   // plusY를 양수로 바꿔서 반대로 움직이게해서 벽에 튕기는것처럼 보이게 한다
@@ -68,18 +71,23 @@ function draw() {
     MovingY = -MovingY;
   }
   // 공이 화면 아래로 향했을때의 코드
-  if (y + MovingY > canvas.height - ballRadius) {
-    alert('Game Over...');
-    // 공의 위치를 시작지점으로 돌린다.
-    resetGame();
-
+  else if (y + MovingY > racketY - ballRadius) {
+    if (y + movingX > canvas.height - ballRadius) {
+      alert('Game Over...');
+      // 공의 위치를 시작지점으로 돌린다.
+      resetGame();
+    }
   }
+
   // (화면 왼쪽을 넘어가거나 화면 오른쪽으로 넘어가거나)
   if (x + movingX > canvas.width - ballRadius || x + movingX < ballRadius) {
     movingX = -movingX;
   }
 
   // 라켓에 주인"공"이 닿았을 때, 벽에 튀긴것처럼 튕기게 하기
+  if (x > racketX && x < racketX + racketWidth && y + ballRadius > racketY) {
+      MovingY = -MovingY;
+  }
 
   // 좌우 각 방향키를 누르면 10의 속도(10px)로 움직인다. 라켓이 벽을 넘지않는 선에서
   if (rightKey && racketX < canvas.width - racketWidth) {
