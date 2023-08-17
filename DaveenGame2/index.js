fb51f67681f9068b233248f0dba5b8eeb0a62cf8
daveen99 <prospecs1039@daum.net>
2023-08-17 15:59:59 +0900

    공에 닿으면 사라지는 벽돌만들기(배열)

--- DaveenGame2/index.js
+++ DaveenGame2/index.js
@@ -1,7 +1,7 @@
 // ============ 변수 모음zip ============ //
 // 캔버스 위에 그래픽을 렌더링하기 위해서
 // 캔버스 엘리먼트에 대한 참조를 [ canvas ] 변수에 저장함
-// 그림을 그리는 실질적 도구인 2D rendering context를 [ ctx ]변수에 저장함
+// 그림을 그리는 실질적 도구인 2D rendering ctx를 [ ctx ]변수에 저장함
 const canvas = document.getElementById('Canvas');
 const ctx = canvas.getContext('2d');
 
@@ -27,8 +27,27 @@ let racketY = canvas.height - 40;
 let rightKey = false;
 let leftKey = false;
 
-// ============ 주인"공"을 캔버스 위에 그리는 파트 ============ //
+// 벽돌의 행과 열
+const blockRow = 7;
+const blockColumn = 6;
+// 벽돌의 넓이와 높이
+const blockWidth = 75;
+const blockHeight = 20;
+// 벽돌 추워서 패딩입음
+const blockPadding = 10;
+const blockOffsetLeft = 30;
+const blockOffsetTop = 30;
 
+// 벽돌 배열을 초기화함 status: 1  <<== 1이면 그림, 0이면 안그림(삭제)
+const blocks = [];
+for (let i = 0; i < blockColumn; i++) {
+  blocks[i] = [];
+  for (let z = 0; z < blockRow; z++) {
+    blocks[i][z] = { x: 0, y: 0, status: 1 };
+  }
+}
+
+// ============ 주인"공"을 캔버스 위에 그리는 파트 ============ //
 // 주인"공"을 그리는 함수
 function drawBall() {
   ctx.beginPath(); // 그리기 시작
@@ -41,10 +60,64 @@ function drawBall() {
 // 라켓을 그리는 함수
 function drawRacket() {
   ctx.beginPath();
+  ctx.shadowBlur = 10; // 그림자의 흐림 정도
+  ctx.shadowColor = 'rgba(0, 0, 0, 0.5)'; // 그림자 색상
+  ctx.shadowOffsetX = 5; // 그림자의 X축 오프셋
+  ctx.shadowOffsetY = 5; // 그림자의 Y축 오프셋
   ctx.rect(racketX, racketY, racketWidth, racketHeight);
   ctx.fillStyle = '#0095DD';
   ctx.fill();
   ctx.closePath();
+  ctx.shadowBlur = 0;
+ctx.shadowColor = 'transparent';
+
+}
+
+// for (let i = 0; i < blockColumn; i++) {
+//   blocks[i] = [];
+
+//   for (let z = 0; z < blockRow; z++) {
+//       blocks[i][z] = { x: 0, y: 0 };
+//   }
+// }
+
+// 파괴할 오브젝트(블록)을 그리는 함수
+function makeBlock() {
+  for (let i = 0; i < blockColumn; i++) {
+    for (let z = 0; z < blockRow; z++) {
+      if (blocks[i][z].status === 1) {
+        let blockX = i * (blockWidth + blockPadding) + blockOffsetLeft;
+        let blockY = z * (blockHeight + blockPadding) + blockOffsetTop;
+        blocks[i][z].x = blockX;
+        blocks[i][z].y = blockY;
+
+        ctx.beginPath();
+        ctx.rect(blockX, blockY, blockWidth, blockHeight);
+        ctx.fillStyle = '#0095DD';
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
+}
+
+function blockDelete() {
+  for (let i = 0; i < blockColumn; i++) {
+    for (let z = 0; z < blockRow; z++) {
+      const target = blocks[i][z];
+      if (target.status === 1) {
+        if (
+          x > target.x &&
+          x < target.x + blockWidth &&
+          y + ballRadius > target.y &&
+          y < target.y + blockHeight
+        ) {
+          MovingY = -MovingY;
+          target.status = 0;
+        }
+      }
+    }
+  }
 }
 
 // 게임을 리셋하는 함수
@@ -56,13 +129,22 @@ function resetGame() {
 
   racketX = (canvas.width - racketWidth) / 2; // canvas의 길이에서 라켓길이를 빼고 그것의 반절의 위치
   racketY = canvas.height - 40;
+
+  for (let i = 0; i < blockColumn; i++) {
+    for (let z = 0; z < blockRow; z++) {
+      blocks[i][z].status = 1;
+    }
+  }
 }
 
 // 그리기 함수 (10밀리초마다 호출됨 = 무한작동)
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // 공을 그리기 전에 기존에 공을 지운다
+  makeBlock(); // 블록 그리기 함수
   drawBall(); // 주인"공" 그리기 함수 호출
   drawRacket(); // 라켓 그리기 함수 호출
+  blockDelete();
+
   // ============ 주인"공"을 벽에 튕기게 하는 파트 ============ //
   // 만약에 현재의 높이에서 + MovingY( -2 )를 했을때 0보다 작아지는경우(벽을넘어감)
   // plusY를 양수로 바꿔서 반대로 움직이게해서 벽에 튕기는것처럼 보이게 한다
@@ -74,7 +156,7 @@ function draw() {
   else if (y + MovingY > racketY - ballRadius) {
     if (y + movingX > canvas.height - ballRadius) {
       alert('Game Over...');
-      // 공의 위치를 시작지점으로 돌린다.
+      // 시작지점으로 돌린다.
       resetGame();
     }
   }
@@ -86,7 +168,7 @@ function draw() {
 
   // 라켓에 주인"공"이 닿았을 때, 벽에 튀긴것처럼 튕기게 하기
   if (x > racketX && x < racketX + racketWidth && y + ballRadius > racketY) {
-      MovingY = -MovingY;
+    MovingY = -MovingY;
   }
 
   // 좌우 각 방향키를 누르면 10의 속도(10px)로 움직인다. 라켓이 벽을 넘지않는 선에서
