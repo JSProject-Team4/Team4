1103528b47964e6ed9aa27e229ecd7ef90273143
hyowon <dlgy9714@gmail.com>
2023-08-28 02:23:44 +0900

    필살기 구현 완료

--- lhw/index.js
+++ lhw/index.js
@@ -8,14 +8,14 @@ const $main = document.querySelector(".main");
 const $gamebox = document.querySelector(".gamebox");
 const $bulletbox = document.querySelector(".Ybox");
 const $hp = document.querySelector(".hp");
-const $reloadbox = document.querySelector('.reloadbox');
+const $reloadbox = document.querySelector(".reloadbox");
 const gameMusic = document.getElementById("game-music");
 const gameMusic2 = document.getElementById("game-music2");
-const $thunder = document.querySelector('.thunder');
+const $thunder = document.querySelector(".thunder");
 let enemyDie = false;
 let die = {
   x: 0,
-  y: 0
+  y: 0,
 };
 
 // 크기
@@ -28,7 +28,13 @@ const MAX_BULLETS = 10;
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 
-let bgImage, charecterImg, bulletImage, enemyImage, gameOverImage, itemImage,gunImage;
+let bgImage,
+  charecterImg,
+  bulletImage,
+  enemyImage,
+  gameOverImage,
+  itemImage,
+  gunImage;
 let currentBullets = MAX_BULLETS;
 
 let gameLoopInterval; // 게임 루프의 타이머 변수
@@ -40,7 +46,7 @@ let score = 0;
 let spaceShipX = 210;
 let spaceShipY = HEIGHT - 68;
 // 컨버스 그리기
-let bulletAdd=1;
+let bulletAdd = 1;
 
 let reloadAdd;
 // 창화면시 게임 크기 조절
@@ -49,50 +55,123 @@ window.addEventListener("resize", () => {
   canvas.style.height = `${HEIGHT}px`; // 높이 설정
 });
 // 총알 hp UI
-// 총알 리로드 주기 
-const reloadStart=()=>{
+// 총알 리로드 주기
+const reloadStart = () => {
   $reloadbox.classList.add("animate-reloadbox");
-  reloadAdd=setInterval(() => {
-      currentBullets+=bulletAdd;
-      uiEvent();
+  reloadAdd = setInterval(() => {
+    currentBullets += bulletAdd;
+    uiEvent();
   }, 5000);
 };
-const reloadEnd=()=>{
+const reloadEnd = () => {
   clearInterval(reloadAdd);
   $reloadbox.classList.remove("animate-reloadbox");
 };
-const specialMoveHandler=(event)=>{
-  // for(let i=0;i<enemyList.length;i++){
-  //   c.drawImage(boomImage, enemyList[i].x-160, enemyList[i].y-200);
-  //   enemyDie = true;
-  //   setTimeout(() => {
-  //     enemyDie = false;
-  //   }, 300);
-  // }
-  // for(let i=0;i<enemy2List.length;i++){
-  //   c.drawImage(boomImage, enemy2List[i].x-160, enemy2List[i].y-200);
-  //   enemyDie = true;
-  //   setTimeout(() => {
-  //     enemyDie = false;
-  //   }, 300);
-  // }
-  $thunder.style.display = 'block';
-
-  // 1초 후에 요소를 다시 숨기도록 설정
-  setTimeout(() => {
-      $thunder.style.display = 'none';
-  }, 200);
-
-  
-  
-  let Addscore=enemyList.length;
-  score+=Addscore;
-  enemy2List=[];
-  enemyList=[];
+const specialMoveHandler = (event) => {
+  if (imgCount1 === 5 || imgCount2 === 5 || imgCount3 === 5) {
+    changeSM(false);
+    $thunder.style.display = "block";
+    // 1초 후에 요소를 다시 숨기도록 설정
+    setTimeout(() => {
+      $thunder.style.display = "none";
+    }, 200);
+    let Addscore = enemyList.length;
+    score += Addscore;
+    enemy2List = [];
+    enemyList = [];
+    imgCount3 = 1;
+    imgCount2 = 1;
+  }
 };
 
+let iterationCount = 0;
+let asdf = 0;
+let imgCount1 = 1;
+let imgCount2 = 1;
+let imgCount3 = 1;
+const changeSM = (e) => {
+  const runForLoop = () => {
+    // 에너지 충전 보이기
+    if (true) {
+      if (iterationCount < 4) {
+        const $specialMoveChange1 = document.querySelector(`.c1`);
 
+        $specialMoveChange1.style.backgroundImage = `url('Image/${imgCount1}.png')`;
+        iterationCount++;
+        imgCount1++;
+        if (imgCount1 === 3) {
+          asdf++;
+        }
+      } else if (iterationCount < 8 && asdf === 1) {
+        const $specialMoveChange2 = document.querySelector(`.c2`);
 
+        $specialMoveChange2.style.backgroundImage = `url('Image/${imgCount2}.png')`;
+        iterationCount++;
+        imgCount2++;
+        console.log(imgCount2);
+        if (imgCount2 === 5) {
+          imgCount2 = 0;
+          asdf++;
+        }
+      } else if (iterationCount < 12 && asdf === 2) {
+        const $specialMoveChange3 = document.querySelector(`.c3`);
+        $specialMoveChange3.style.backgroundImage = `url('Image/${imgCount3}.png')`;
+        iterationCount++;
+        imgCount3++;
+        if (imgCount3 === 5) {
+          asdf++;
+        }
+        console.log(`Iteration ${asdf}`);
+      }
+    }
+  };
+
+  if (e === false) {
+    const $specialMoveChange2 = document.querySelector(`.c2`);
+    const $specialMoveChange1 = document.querySelector(`.c1`);
+    const $specialMoveChange3 = document.querySelector(`.c3`);
+    if (asdf === 3) {
+      console.log(asdf, 1);
+      imgCount3 = 1;
+      $specialMoveChange3.style.backgroundImage = `url('Image/${0}.png')`;
+      asdf--;
+      iterationCount = 8;
+    } else if (asdf === 2) {
+      console.log(asdf, 2);
+      imgCount2 = 1;
+      $specialMoveChange3.style.backgroundImage = `url('Image/${0}.png')`;
+      $specialMoveChange2.style.backgroundImage = `url('Image/${0}.png')`;
+      asdf--;
+      iterationCount = 4;
+    } else if (asdf === 1) {
+      console.log(asdf, 3);
+      imgCount1 = 1;
+      $specialMoveChange3.style.backgroundImage = `url('Image/${0}.png')`;
+      $specialMoveChange2.style.backgroundImage = `url('Image/${0}.png')`;
+      $specialMoveChange1.style.backgroundImage = `url('Image/${0}.png')`;
+      asdf--;
+      iterationCount = 0;
+    }
+  }
+  let intervalId = setInterval(runForLoop, 5000); // 1초마다 runForLoop 함수 실행
+  if (e === false) {
+    clearInterval(intervalId);
+  }
+};
+const resetSM = () => {
+  const $specialMoveChange2 = document.querySelector(`.c2`);
+  const $specialMoveChange1 = document.querySelector(`.c1`);
+  const $specialMoveChange3 = document.querySelector(`.c3`);
+  $specialMoveChange3.style.backgroundImage = `url('Image/${0}.png')`;
+  $specialMoveChange2.style.backgroundImage = `url('Image/${0}.png')`;
+  $specialMoveChange1.style.backgroundImage = `url('Image/${0}.png')`;
+
+  iterationCount = 0;
+  asdf = 0;
+  imgCount1 = 1;
+  imgCount2 = 1;
+  imgCount3 = 1;
+};
 const uiEvent = () => {
   function removeAllChildren(parent) {
     while (parent.firstChild) {
@@ -100,9 +179,8 @@ const uiEvent = () => {
     }
   }
   if (!gameStatus) {
-    
     removeAllChildren($bulletbox);
-    
+
     for (let i = 0; i < MAX_BULLETS; i++) {
       const $bullet = document.createElement("img");
       $bullet.src = "Image/bullet.png";
@@ -112,8 +190,8 @@ const uiEvent = () => {
   }
   if (!gameOver) {
     removeAllChildren($bulletbox);
-    if (currentBullets >= 15) {
-      currentBullets = 15;
+    if (currentBullets >= 10) {
+      currentBullets = 10;
     }
     for (let i = 0; i < currentBullets; i++) {
       const $bullet = document.createElement("img");
@@ -169,13 +247,12 @@ function Bullet() {
         enemyDie = true;
         die = {
           x: enemyLeft - 160,
-          y: enemyBottom - 200
+          y: enemyBottom - 200,
         };
 
         setTimeout(() => {
           enemyDie = false;
         }, 100);
-
 
         uiEvent(); // UI 업데이트
       }
@@ -197,7 +274,6 @@ function Bullet() {
           bulletRight > enemyLeft &&
           bulletLeft < enemyRight
         ) {
-          
           hitcount++;
           this.alive = false;
           if (hitcount === 2) {
@@ -205,12 +281,12 @@ function Bullet() {
             enemy2List.splice(i, 1);
             score = score + 2;
             hitcount = 0;
-            enemyDie=true;
+            enemyDie = true;
             die = {
               x: enemyLeft - 160,
-              y: enemyBottom - 200
+              y: enemyBottom - 200,
             };
-    
+
             setTimeout(() => {
               enemyDie = false;
             }, 100);
@@ -283,7 +359,6 @@ const decreaseHp = () => {
 const EnemyRetouch = (value) => {
   if (score <= 5) {
     result = value === true ? 2 : 5;
-    
   } else if (score <= 10) {
     result = value === true ? 3 : 5;
   } else if (score <= 15) {
@@ -291,13 +366,12 @@ const EnemyRetouch = (value) => {
   } else if (score <= 20) {
     result = value === true ? 5 : 6;
     if (!enemy2Spawned) {
-      bulletAdd=2;
+      bulletAdd = 2;
       createEnemy2();
       createEnemy3();
       enemy2Spawned = true; // createEnemy2() 함수가 실행되었음을 표시
     }
   } else if (score <= 25) {
-    
     result = value === true ? 6 : 7;
   } else if (score <= 35) {
     result = value === true ? 6 : 8;
@@ -363,7 +437,6 @@ function Enemy2() {
         reloadEnd();
         pauseMusic();
         clearInterval(einterval2);
-
       }
     }
   };
@@ -393,10 +466,10 @@ const loadImage = () => {
   enemy2Image = new Image();
   enemy2Image.src = "Image/enemy2.png";
 
-  gunImage= new Image();
+  gunImage = new Image();
   gunImage.src = "Image/gun.png";
-  
-  boomImage= new Image();
+
+  boomImage = new Image();
   boomImage.src = "Image/unscreen.gif";
 };
 const createBullet = () => {
@@ -434,7 +507,7 @@ function createEnemy3() {
       if (gameStatus) {
         let e2 = new Enemy(); // 새로운 적 객체 생성
         e2.init();
-        console.log('test');
+        console.log("test");
       }
     }, 3000);
   }
@@ -452,8 +525,6 @@ const createItem = () => {
     }
   }, 5000);
 };
-
-
 
 // 음악 재생
 function playMusic() {
@@ -474,7 +545,7 @@ let keysDown = {};
 const keyboardListener = () => {
   document.addEventListener("keydown", (e) => {
     keysDown[e.keyCode] = true;
-    if(e.keyCode===16){
+    if (e.keyCode === 16) {
       specialMoveHandler(e);
     }
   });
@@ -484,7 +555,7 @@ const keyboardListener = () => {
       createBullet();
       uiEvent();
     }
-    if(e.keyCode===16){
+    if (e.keyCode === 16) {
     }
   });
 };
@@ -552,12 +623,10 @@ const rederHendler = () => {
   } //적 추가
   for (let i = 0; i < enemyList.length; i++) {
     c.drawImage(enemyImage, enemyList[i].x, enemyList[i].y);
-
   }
   for (let i = 0; i < enemy2List.length; i++) {
     c.drawImage(enemy2Image, enemy2List[i].x, enemy2List[i].y); // 에너미2 이미지 그리기
   }
-
 };
 
 const startEvent = () => {
@@ -565,6 +634,7 @@ const startEvent = () => {
     if (!gameOver) {
       playMusic();
       reloadStart();
+      changeSM(true);
       $backDrop.style.display = "none";
       $gameStart.style.display = "none";
       gameStatus = true;
@@ -584,7 +654,7 @@ const restartGame = () => {
   enemyList = [];
   enemy2List = [];
   itemList = [];
-  
+
   enemy2Spawned = false; // enemy2Spawned 변수 초기화
 
   clearInterval(gameLoopInterval); // 기존의 게임 루프 중단
@@ -597,6 +667,11 @@ const restartGame = () => {
       $backDrop.style.display = "block";
 
       $gameOver.addEventListener("click", () => {
+        if (!gameStatus) {
+          clearInterval(intervalId);
+        }
+        resetSM();
+
         hpcount = 0; // hp 초기화
         $hp.style.height = "100%";
         uiEvent();
@@ -609,7 +684,6 @@ const restartGame = () => {
         saveScore = score;
         restartGame();
         uiEvent();
-
       });
     } else {
       // 게임 루프 내용 실행
@@ -619,7 +693,6 @@ const restartGame = () => {
     }
   }, 1000 / 60); // 60FPS에 가까운 속도로 실행하도록 설정
 };
-
 
 loadImage();
 keyboardListener();
